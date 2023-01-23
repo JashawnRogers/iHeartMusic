1ee87ce428e98cb727f29890152a041c9af927f2
Jashawn <rogersjashawn20@gmail.com>
2023-01-22 21:38:09 -0700

    Created server login route

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const app = express();
+
+app.post("/login", (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "6c3198a53fac4098ad2067196e71b282",
+    clientSecret: "2fc9fe34bd604349a4ffbff53f11e5b1",
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
